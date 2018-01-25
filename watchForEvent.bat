116ce0f93cc3176c0c067eb6af6d8550cf7bfddc
jamie <jamie.hatfield@issinc.com>
2018-01-25 12:49:08 -0700

    Updated to clear token count and ensure it is pulling token counts from the active event

--- watchForEvent.bat
+++ watchForEvent.bat
@@ -3,15 +3,16 @@
 set server=%1
 set user=%2
 set hash=%3
-
+set tokens=
 
 :TOP
 for /f %%a in ('powershell -Command "Get-Date -format yyyyMMdd_HHmmss"') do set TIMESTAMP=%%a
 
-curl -s http://%server%.djartsgames.ca/~idle/post.php?call=getUserDetails^&instance_key=0^&user_id=%user%^&hash=%hash% | \apps\jq\jq-win64.exe .details.event_details[0].user_data.event_tokens > _ud.tmp
+set jqCmd=.details.event_details[] ^| select(.active=="true") ^| .user_data.event_tokens
+curl -s http://%server%.djartsgames.ca/~idle/post.php?call=getUserDetails^&instance_key=0^&user_id=%user%^&hash=%hash% | \apps\jq\jq-win64.exe "%jqCmd%" > _ud.tmp
 set /p tokens=<_ud.tmp
 del _ud.tmp
-if NOT "%tokens%" == "null" (
+if NOT "%tokens%" == "" (
   echo %TIMESTAMP% Event is live %tokens%
   \apps\toaster\toast\bin\release\toast.exe -t "EVENT IS LIVE!!!" -m "GO GO GO!"
 )
